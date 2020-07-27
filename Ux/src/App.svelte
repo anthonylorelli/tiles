fee16bfec5217159997e64acfe7e54cdd28f7f25
sstchur <47366639+sstchur@users.noreply.github.com>
2020-07-26 17:04:48 -0700

    starter ux project

---
+++ Ux/src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let name: string;
+
+	import Avett from './TheAvettBrothers.svelte';
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>This project by Anthony and Stephen is just getting started so there's really nothing interesting here yet.</p>
+
+	<p>But fear not, we've got this awesome Avett Brothers component for you to look at, demonstrating a tiny bit of Svelte</p>
+
+	<Avett />
+</main>
+
+<style>
+	main
+	{
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1
+	{
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px)
+	{
+		main
+		{
+			max-width: none;
+		}
+	}
+</style>
---
+++ Ux/src/TheAvettBrothers.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    interface IMusician
+    {
+        name: string;
+        instrument: string;
+    }
+
+    const theAvettBrothers: IMusician[] =
+    [
+        { name: 'Scott Avett', instrument: 'banjo' },
+        { name: 'Seth Avett', instrument: 'guitar' },
+        { name: 'Bob Crawford', instrument: 'bass' },
+        { name: 'Joe Kwon', instrument: 'cello' },
+        { name: 'Mike Marsh', instrument: 'drums' },
+        { name: 'Tania Elizabeth', instrument: 'violin' }
+    ];
+</script>
+
+<h1>The Avett Brothers!</h1>
+
+<ul>
+    { #each theAvettBrothers as musician }
+        <li><b>{ musician.name } - lord of the { musician.instrument }</b></li>
+    { /each }
+</ul>
