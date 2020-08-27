65d40643e70b95ead5d97d08a9110911e8065fa0
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-08-26 21:08:52 -0700

    Add ability to pick random color

--- services/experiment/ColorPattern.cs
+++ services/experiment/ColorPattern.cs
@@ -9,5 +9,13 @@ namespace experiment
         public Color East { get; set; }
         public Color South { get; set; }
         public Color West { get; set; }
+
+        public static Color PickRandom()
+        {
+            var random = new Random();
+            var bytes = new byte[3];
+            random.NextBytes(bytes);
+            return Color.FromArgb(bytes[0], bytes[1], bytes[2]);
+        }
     }
 }
