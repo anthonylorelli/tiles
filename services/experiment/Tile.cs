3fa10a4f919a5722d61aec2e8b0c31842c694354
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-09-04 20:55:52 -0700

    Add getters/setters for Tile colors

--- services/experiment/Tile.cs
+++ services/experiment/Tile.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Drawing;
 
 namespace experiment
 {
     public class Tile
     {
-        public ColorPattern Pattern { get; set; }
+        public Color North { get; set; }
+        public Color East { get; set; }
+        public Color South { get; set; }
+        public Color West { get; set; }
         public string Id { get; set; }
     }
 }
